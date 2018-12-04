e948ddae8b535df530d0a977c4c42aa86fb93c90
Fedchenkov Petr <giggsoff@gmail.com>
2018-12-04 18:24:29 +0300

    design WIP

--- app/views/First.js
+++ app/views/First.js
@@ -8,7 +8,7 @@ class First extends Component {
     this.state = {
       items: [
         {
-          name: 'TURQUOISE', code: '#1abc9c', polyline: [
+          name: 'TURQUOISE', code: '#1abc9c', date: '05:13 4 Dec 2018', score: 56, polyline: [
             {
               latitude: 59.904435,
               longitude: 30.307153
@@ -24,7 +24,7 @@ class First extends Component {
           ]
         },
         {
-          name: 'EMERALD', code: '#2ecc71', polyline: [
+          name: 'EMERALD', code: '#2ecc71', date: '09:13 4 Dec 2018', score: 78, polyline: [
             {
               latitude: 59.914435,
               longitude: 30.317153
@@ -40,7 +40,7 @@ class First extends Component {
           ]
         },
         {
-          name: 'PETER RIVER', code: '#3498db', polyline: [
+          name: 'PETER RIVER', code: '#3498db', date: '05:13 3 Dec 2018', score: 93, polyline: [
             {
               latitude: 59.914435,
               longitude: 30.357153
@@ -55,7 +55,7 @@ class First extends Component {
             }
           ]
         }, {
-          name: 'AMETHYST', code: '#9b59b6', polyline: [
+          name: 'AMETHYST', code: '#9b59b6', date: '05:13 2 Dec 2018', score: 82, polyline: [
             {
               latitude: 59.924435,
               longitude: 30.307153
@@ -71,7 +71,7 @@ class First extends Component {
           ]
         },
         {
-          name: 'WET ASPHALT', code: '#34495e', polyline: [
+          name: 'WET ASPHALT', code: '#34495e', date: '05:13 1 Dec 2018', score: 76, polyline: [
             {
               latitude: 59.904445,
               longitude: 30.301153
@@ -90,23 +90,27 @@ class First extends Component {
     };
   }
 
-  _onPressButton = (name, polyline) => {
-    console.warn(name);
-    this.props.navigation.navigate('Map', {polyline: [polyline]});
+  _onPressButton = (item) => {
+    this.props.navigation.navigate('Map', {polyline: [item.polyline]});
   };
 
   render() {
     return (
       <GridView
-        itemDimension={250}
+        itemDimension={150}
         items={this.state.items}
         style={styles.gridView}
         renderItem={item => (
           <View style={[styles.itemContainer, {backgroundColor: item.code}]}>
-            <Text style={styles.itemName}>{item.name}</Text>
-            <Text style={styles.itemCode}>{item.code}</Text>
-            <Button style={styles.itemButton} onPress={() => this._onPressButton(item.name, item.polyline)}
-                    title="Learn More"/>
+            <View style={[styles.itemContainer1]}>
+              <Text style={styles.itemScore}>Your score: {item.score}</Text>
+            </View>
+            <View style={[styles.itemContainer2]}>
+              <Text style={styles.itemName}>{item.name}</Text>
+              <Text style={styles.itemDate}>{item.date}</Text>
+              <Button style={styles.itemButton} onPress={() => this._onPressButton(item)}
+                      title="Learn More"/>
+            </View>
           </View>
         )}
       />
@@ -119,8 +123,16 @@ const styles = StyleSheet.create({
     paddingTop: 25,
     flex: 1,
   },
-  itemContainer: {
+  itemContainer1: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60
+  },
+  itemContainer2: {
     justifyContent: 'flex-end',
+  },
+  itemContainer: {
+    flex:1,
     borderRadius: 5,
     padding: 10,
     height: 150,
@@ -130,7 +142,12 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontWeight: '600',
   },
-  itemCode: {
+  itemScore: {
+    fontSize: 19,
+    color: '#fff',
+    fontWeight: '800',
+  },
+  itemDate: {
     fontWeight: '600',
     fontSize: 12,
     color: '#fff',
